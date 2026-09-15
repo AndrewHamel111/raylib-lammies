AndrewHamel111/raylib-lammies

commit 57d7a591d750302b9c8705b288a98a5cf2c43b72

Cards and Objects ordered in separate array this resolves issues where changing order of objects on screen would invalidate any pointers, would become a serious problem on a full game

src/object/management.c
@@ -3,7 +3,10 @@
 #include "debug.h"
 #include "cursor.h"
 
-Object objects[MAX_OBJECTS] = {0};
+/// Raw memory block objects are allocated from
+Object objects_internal[MAX_OBJECTS] = {0};
+/// Ordered list with higher indices being higher off the table.
+Object* objects[MAX_OBJECTS] = {0};
 int objects_count = 0;
 static int next_id = 1;
 
@@ -14,13 +17,14 @@ Object* ObjectConstruct(void)
 {
 	for (int i = 0; i < MAX_OBJECTS; i++)
 	{
-		Object* o = objects + i;
+		Object* o = objects_internal + i;
 		if (o->id) continue;
 
 		*o = (Object){0};
-		o->id = next_id;
+		o->id = next_id++;
+
+		objects[objects_count] = o;
 		objects_count++;
-		next_id++;
 
 		return o;
 	}
@@ -29,25 +33,25 @@ Object* ObjectConstruct(void)
 	return NULL;
 }
 
-//void ObjectFree(const Object* object)
-//{
-//	Object* o = ObjectGet(object->id);
-//	if (!o)
-//	{
-//		TraceLog(LOG_ERROR, "ObjectFree failed when no object matching the specified object's ID was present");
-//		return;
-//	}
-//
-//	o->id = 0;
-//}
+void ObjectFree(const Object* object)
+{
+	Object* o = ObjectGet(object->id);
+	if (!o || o != object)
+	{
+		TraceLog(LOG_ERROR, "ObjectFree failed when no object matching the specified object's ID was present, or the provided pointer did not match the region in memory!");
+		return;
+	}
+
+	*o = (Object){0};
+}
 
 Object* ObjectGet(int id)
 {
-	for (int i = 0; i < objects_count; i++)
+	for (int i = 0; i < MAX_OBJECTS; i++)
 	{
-		if (objects[i].id != id) continue;
+		if (objects_internal[i].id != id) continue;
 
-		return objects + i;
+		return objects_internal + i;
 	}
 
 	TraceLog(LOG_WARNING, "ObjectGet could not find object with ID #%d", id);
@@ -55,6 +59,12 @@ Object* ObjectGet(int id)
 }
 
 Object* ObjectsGet(int* count)
+{
+	*count = MAX_OBJECTS;
+	return objects_internal;
+}
+
+Object** ObjectsGetOrdered(int* count)
 {
 	*count = objects_count;
 	return objects;
@@ -71,28 +81,28 @@ void ObjectsDraw(void)
 
 	for (int i = 0; i < objects_count; i++)
 	{
-		if (!objects[i].id) continue;
+		if (!objects[i]->id) continue;
 
-		if (held_object == objects + i)
+		if (held_object == objects[i])
 		{
 			if (CursorHeightGet() == CursorHeightObject)
 			{
 				CursorDraw();
 			}
-			ObjectDrawShadowed(objects + i);
+			ObjectDrawShadowed(objects[i]);
 		}
-		else if (picked_object == objects + i)
+		else if (picked_object == objects[i])
 		{
-			ObjectDrawHighlight(objects + i, OBJECT_DEFAULT_HIGHLIGHT);
+			ObjectDrawHighlight(objects[i], OBJECT_DEFAULT_HIGHLIGHT);
 		}
 		else
 		{
-			ObjectDraw(objects + i);
+			ObjectDraw(objects[i]);
 		}
 
 		if (drawHitboxes)
 		{
-			DrawRectangleRec(ObjectRect(objects + i), RED);
+			DrawRectangleRec(ObjectRect(objects[i]), RED);
 		}
 	}
 }
@@ -107,7 +117,7 @@ Object* MousePickObjectExcluding(Vector2 mpos, const Object* excluded)
 	Object* pickedObject = NULL;
 	for (int i = 0; i < objects_count; i++)
 	{
-		Object* o = objects + i;
+		Object* o = objects[i];
 		if (!o->id || o == excluded) continue;
 
 		Rectangle rect = ObjectRect(o);
@@ -135,7 +145,7 @@ static int GetObjectIndex(const Object* object)
 {
 	for (int i = 0; i < objects_count; i++)
 	{
-		if (object->id != objects[i].id) continue;
+		if (object->id != objects[i]->id) continue;
 
 		return i;
 	}
@@ -144,47 +154,51 @@ static int GetObjectIndex(const Object* object)
 	return -1;
 }
 
-void DeleteObject(Object *object)
+void RemoveObject(Object *object)
 {
 	int idx = GetObjectIndex(object);
-	DeleteObjectAt(idx);
+	RemoveObjectAt(idx);
 }
 
-void DeleteObjectAt(int index)
+Object* RemoveObjectAt(int index)
 {
-	if (index < 0 || index >= MAX_OBJECTS)
+	if (index < 0 || index >= objects_count)
 	{
-		TraceLog(LOG_WARNING, "DeleteObjectAt failed: invalid index");
-		return;
+		TraceLog(LOG_WARNING, "RemoveObjectAt failed: invalid index");
+		return NULL;
 	}
+
+	Object* object = objects[index];
 
 	if (objects_count == 1)
 	{
-		objects[0] = (Object){0};
+		objects[0] = NULL;
 	}
 	else if (index == (objects_count - 1))
 	{
-		objects[index] = (Object){0};
+		objects[index] = NULL;
 	}
 	else
 	{
 		size_t sz = (objects_count - 1 - index) * sizeof(Object);
 		if (sz <= 0)
 		{
-			TraceLog(LOG_ERROR, "DeleteObjectAt: Invaild sz");
+			TraceLog(LOG_ERROR, "RemoveObjectAt: Invalid sz");
 		}
 		memmove(objects + index, objects + index + 1, sz);
-		objects[objects_count - 1] = (Object){0};
+		objects[objects_count - 1] = NULL;
 	}
 
 	objects_count--;
+	return object;
 }
 
 void DeleteAllObjects(void)
 {
 	for (int i = 0; i < MAX_CARDS; i++)
 	{
-		objects[i] = (Object){0};
+		objects_internal[i] = (Object){0};
+		objects[i] = NULL;
 	}
 	objects_count = 0;
 }
@@ -209,11 +223,10 @@ Object* MoveObjectAtToTop(int index)
 		return NULL;
 	}
 
-	Object copy = objects[index];
-	DeleteObjectAt(index);
+	Object* value = RemoveObjectAt(index);
 
-	objects[objects_count] = copy;
+	objects[objects_count] = value;
 	objects_count++;
 
-	return objects + objects_count - 1;
+	return value;
 }

src/card/list.c
@@ -1,15 +1,23 @@
 #include <string.h>
 #include "list.h"
 #include "constants.h"
+#include "utility.h"
 
-static Card cards[MAX_CARDS] = {0};
+static Card cards_internal[MAX_CARDS] = {0};
+static Card* cards[MAX_CARDS] = {0};
 static int cards_count = 0;
 static int next_id = 1;
 
 Card* GetCards(int* outCount)
 {
-    *outCount = cards_count;
-    return cards;
+    *outCount = MAX_CARDS;
+    return cards_internal;
+}
+
+Card** GetCardsOrdered(int* outCount)
+{
+	*outCount = cards_count;
+	return cards;
 }
 
 Card* CreateCard(Vector2 position, Suit suit, Rank rank)
@@ -20,8 +28,14 @@ Card* CreateCard(Vector2 position, Suit suit, Rank rank)
         return NULL;
     }
 
-    Card* card = cards + cards_count;
-    cards_count++;
+	Card* card = NULL;
+	for (int i = 0; i < MAX_CARDS; i++)
+	{
+		if (cards_internal[i].id) continue;
+
+		card = cards_internal + i;
+		break;
+	}
 
 	card->id = next_id++;
     card->_position = position;
@@ -29,40 +43,28 @@ Card* CreateCard(Vector2 position, Suit suit, Rank rank)
     card->rank = rank;
     card->color = ColorFromHSV((float)GetRandomValue(0, 359), 0.2f, 0.8f);
 
+	cards[cards_count] = card;
+	cards_count++;
+
     return card;
-}
-
-Card* AddCard(Card card)
-{
-    if (cards_count == MAX_CARDS - 1)
-    {
-        TraceLog(LOG_ERROR, "Cannot add card to card list as cards_count is at MAX_CARDS");
-        return NULL;
-    }
-
-	Card* newCard = cards + cards_count;
-    *newCard = card;
-	newCard->id = next_id++;
-    cards_count++;
-	return newCard;
 }
 
 Card* AddCardValue(unsigned int value)
 {
-	Card card = {.rank = value % 13, .suit = value / 13};
-	return AddCard(card);
+	return CreateCard(V(0,0), value % 13, value / 13);
 }
 
 static int GetCardIndex(Card* card)
 {
-	size_t idx = ((long)card - (long)cards) / sizeof(Card);
-	if (idx >= MAX_CARDS)
+	for (int i = 0; i < cards_count; i++)
 	{
-		TraceLog(LOG_WARNING, "GetCardIndex failed");
-		return -1;
+		if (cards[i] != card) continue;
+
+		return i;
 	}
 
-	return (int)idx;
+	TraceLog(LOG_ERROR, "GetCardIndex failed");
+	return -1;
 }
 
 void DeleteCard(Card *card)
@@ -74,26 +76,33 @@ void DeleteCard(Card *card)
 		return;
 	}
 
-	DeleteCardAt(idx);
+	RemoveCardAt(idx);
+
+	*card = (Card){0};
 }
 
-void DeleteCardAt(int index)
+Card* RemoveCardAt(int index)
 {
 	if (index < 0 || index >= cards_count)
 	{
-		TraceLog(LOG_WARNING, "DeleteCardAt failed: invalid index");
-		return;
+		TraceLog(LOG_WARNING, "RemoveCardAt failed: invalid index");
+		return NULL;
 	}
+
+	Card* card = cards[index];
 
 	memmove(cards + index, cards + index + 1, (cards_count - 1 - index) * sizeof(Card));
 	cards_count--;
+
+	return card;
 }
 
 void DeleteAllCards(void)
 {
 	for (int i = 0; i < MAX_CARDS; i++)
 	{
-		cards[i] = (Card){0};
+		cards_internal[i] = (Card){0};
+		cards[i] = NULL;
 	}
 	cards_count = 0;
 }
@@ -118,13 +127,12 @@ Card* MoveCardAtToTop(int index)
 		return NULL;
 	}
 
-	Card copy = cards[index];
-	DeleteCardAt(index);
+	Card* value = RemoveCardAt(index);
 
-	cards[cards_count] = copy;
+	cards[cards_count] = value;
 	cards_count++;
 
-	return cards + cards_count - 1;
+	return cards[cards_count - 1];
 }
 
 Card* MousePickCard(Vector2 mpos)
@@ -138,7 +146,7 @@ Card* MousePickCardExcluding(Vector2 mpos, Card* excluded)
 
 	for (int i = 0; i < cards_count; ++i)
 	{
-		Card* card = cards + i;
+		Card* card = cards[i];
 		if (!card->_locked && CheckCollisionPointRec(mpos, CardGetRect(card)) && card != excluded)
 		{
 			heldCard = card;

src/card/list.h
@@ -3,11 +3,11 @@
 #include "card.h"
 
 Card* GetCards(int* outCount);
+Card** GetCardsOrdered(int* outCount);
 Card* CreateCard(Vector2 position, Suit suit, Rank rank);
-Card* AddCard(Card card);
 Card* AddCardValue(unsigned int value);
 void DeleteCard(Card* card);
-void DeleteCardAt(int index);
+Card* RemoveCardAt(int index);
 void DeleteAllCards(void);
 Card* MoveCardToTop(Card* card);
 Card* MoveCardAtToTop(int index);

src/card/manager.c
@@ -194,13 +194,14 @@ void CardManagerUpdate(float ft)
 			}
 			else
 			{
+				// TODO: switch to ordered list if there's any problems
 				int objectsCount;
-				Object* objects = ObjectsGet(&objectsCount);
+				Object** objects = ObjectsGetOrdered(&objectsCount);
 				for (int i = 0; i < objectsCount && check; i++)
 				{
-					if (!objects[i].id || objects + i == held_object) continue;
+					if (!objects[i]->id || objects[i] == held_object) continue;
 
-					if (!CheckCollisionRecs(heldObjectRec, ObjectRect(objects + i))) continue;
+					if (!CheckCollisionRecs(heldObjectRec, ObjectRect(objects[i]))) continue;
 
 					check = false;
 				}
@@ -320,7 +321,9 @@ void CardManagerUpdate(float ft)
 		Card* cards = GetCards(&count);
 		for (int i = count - 1; i >= 0; i--)
 		{
-			DeleteCard(cards + i);
+			Card* card = cards + i;
+			DeleteCard(card);
+			//ObjectFree(card); // TODO: use this once Cards are objects, as Delete will become Remove and thus will not delete the inner object!
 			ReturnCardTo(&main_deck, cards[i], DeckBottom);
 		}
 	}
@@ -341,10 +344,10 @@ void CardManagerDrawAllCards(void)
 {
 	float ft = GetFrameTime();
     int cardsCount = 0;
-    Card* cards = GetCards(&cardsCount);
+    Card** cards = GetCardsOrdered(&cardsCount);
     for (int i = 0; i < cardsCount; ++i)
     {
-        Card* card = cards + i;
+        Card* card = cards[i];
 		float alpha = card->_locked ? 0.6f : 1.0f;
 		if (held_card == card)
 		{

src/game.c
@@ -119,12 +119,13 @@ void GameLoop(void)
 			float fontSize = 32;
 			int spacer = 4;
 			int objectsCount;
-			Object* objects = ObjectsGet(&objectsCount);
+			Object** objects = ObjectsGetOrdered(&objectsCount);
 			DrawRectangleRec(R(0, 0, 320, (fontSize + spacer) * (objectsCount + 4)), Fade(BLACK, 0.75f));
 			for (int i = 0; i < objectsCount + 4; i++)
 			{
 				Color color = i < objectsCount ? WHITE : GRAY;
-				DrawTextEx(GetMonoFont(), TextFormat("[%d]: #%d", i, objects[i].id), V(spacer, spacer + (i * (fontSize + spacer))), fontSize, 1.0f, color);
+				int id = objects[i] ? objects[i]->id : 0;
+				DrawTextEx(GetMonoFont(), TextFormat("[%d]: #%d", i, id), V(spacer, spacer + (i * (fontSize + spacer))), fontSize, 1.0f, color);
 			}
 		}
 //        DrawFPS(10, 10);

src/object/discard/discard.c
@@ -123,16 +123,17 @@ Card* ObjectDiscardPop(Object* object)
 
 	Deck* deck = &(object->data.discard.deck);
 
-	Card card = DrawNewCard(deck);
-	card._position = object->_position;
-	card._animationState = CardStateDefault;
-	card._faceUp = true;
+	Card* card = AddCardValue(DrawNewCardValue(deck));
+	card->_position = object->_position;
+	card->_animationState = CardStateDefault;
+	card->_faceUp = true;
 
 	// Destroy discard if the last card is popped
 	if (GetDeckCount(deck) == 0)
 	{
-		DeleteObject(object);
+		RemoveObject(object);
+//		ObjectFree(object);
 	}
 
-	return AddCard(card);
+	return card;
 }

src/object/management.h
@@ -4,10 +4,11 @@
 
 /// Returns a pointer to an available Object
 Object* ObjectConstruct(void);
-//void ObjectFree(const Object* object);
+void ObjectFree(const Object* object);
 
 Object* ObjectGet(int id);
 Object* ObjectsGet(int* count);
+Object** ObjectsGetOrdered(int* count);
 
 void ObjectsTick(float ft);
 void ObjectsDraw(void);
@@ -19,8 +20,9 @@ void ObjectSetHeld(const Object* object);
 void ObjectSetPicked(const Object* object);
 
 // List stuff
-void DeleteObject(Object* object);
-void DeleteObjectAt(int index);
+void RemoveObject(Object* object);
+/// Remove the specified object from it's place on the "table", the object is NOT DELETED until ObjectFree is called.
+Object* RemoveObjectAt(int index);
 void DeleteAllObjects(void);
 
 Object* MoveObjectToTop(Object* object);

src/object/object.c
@@ -102,7 +102,7 @@ bool ObjectCombine(Object* source, Object* destination)
 	{
 		memmove(destDeck->arr + destDeck->count, sourceDeck->arr, sourceDeckCount * sizeof(uint));
 		destDeck->count += sourceDeckCount;
-		DeleteObject(source);
+		RemoveObject(source);
 		return true;
 	}
 	else if (DeckIsFull(destDeck))

commit 3b481eba1662372f99bf5798f13f9cac8a23bd4b

Fix for crazy bug introduced in 57d7a591

src/object/management.c
@@ -197,7 +197,7 @@ Object* RemoveObjectAt(int index)
 	}
 	else
 	{
-		size_t sz = (objects_count - 1 - index) * sizeof(Object);
+		size_t sz = (objects_count - 1 - index) * sizeof(Object*);
 		if (sz <= 0)
 		{
 			TraceLog(LOG_ERROR, "RemoveObjectAt: Invalid sz");